21c113079ed183dec754f3193a0829c3a54b2cad
VadimVadimych <97958589+VadimVadimych@users.noreply.github.com>
2026-03-26 13:39:52 +0300

    Update prtg2pd.ps1
    Removed unnecessary parameters
    Added a line for the security protocol

--- prtg2pd.ps1
+++ prtg2pd.ps1
@@ -1,18 +1,15 @@
-# Powershell script to trigger and resolve PRTG alerts in PagerDuty using the events v2 API
+# Powershell script to trigger PRTG alerts in Pachka messenger 
 
 # Notes
 # Copy the line below (exluding the hash) into the parameters field in the PRTG notification template
-# '%probe' '%device' '%deviceid' '%sensor' '%sensorid' '%group' '%groupid' '%home' '%host' '%status' '%colorofstate' '%down' '%priority' '%message' '%comments' '%datetime'
+# '%probe' '%device' '%sensor' '%group' '%home' '%host' '%status' '%colorofstate' '%down' '%priority' '%message' '%datetime'
 
 # Ingest the alert payload from PRTG
 Param(
     [string]$probe,
     [string]$device,
-    [string]$deviceid,
     [string]$sensor,
-    [string]$sensorid,
     [string]$group,
-    [string]$groupid,
     [string]$prtg_home,
     [string]$prtg_host,
     [string]$status,
@@ -20,78 +17,54 @@ Param(
     [string]$down,
     [string]$priority,
     [string]$message,
-    [string]$comments,
     [string]$datetime
 )
 
-# Determine the Event Action
-$regex = [regex] "\((.*)\)"
-$action = $regex::match($status, $regex).groups[1]
 
-switch ($action) {
-    "now: Up" { $PDevent = "resolve" }
-    default { $PDevent = "trigger" }
-}
 
 # Determine the Severity
 switch ($colorofstate) {
     "#b4cc38"	{ $Severity = "info" }
     "#ffcb05"	{ $Severity = "warning" }
-    #   "Error"		{$Severity="error"}
+    "#808282"	{ $Severity ="unknown"}
     "#d71920"	{ $Severity = "critical" }
     default { $Severity = "critical" }
 }
 
-$RoutingKey = "<INSERT ROUTING KEY HERE>"
-$Url = "https://events.pagerduty.com/v2/enqueue"
 
-$Description = "$device $sensor $status $down"
-# $Severity = "critical"
-$Timestamp = Get-Date -UFormat "%Y-%m-%dT%T%Z"
+$Url = "<here Webhook URL from Pachka (Пачка)>"
 
-$AlertPayload = @{
-    routing_key  = $RoutingKey
-    event_action = $PDevent
-    dedup_key    = $sensor
-    client       = "PRTG Network Monitor"
-    client_url   = $prtg_home
-    payload      = @{
-        summary        = $Description
-        timestamp      = $Timestamp
-        source         = $device
-        severity       = $Severity
-        component      = $group
-        class          = $sensor
-        custom_details = @{
+
+
+$AlertPayload = [ordered]@{
             prtg_server  = $prtg_home
             probe        = $probe
-            group        = $group, $groupid
-            device       = $device, $deviceid
-            sensor       = $sensor, $sensorid
-            url          = $prtg_host
-            colorofstate = $colorofstate
+            group        = $group
+            device       = $device
+            sensor       = $sensor
+            status       = $status
+	        url          = $prtg_host
+            severity     = $Severity
             down         = $down
-            downtime     = $downtime
             priority     = $priority
             message      = $message
             datetime     = $datetime
-            comments     = $comments
-            status       = $status
+            
         }
-    }
-}
 
 # Convert Events Payload to JSON
 
 $json = ConvertTo-Json -InputObject $AlertPayload
 
-$logEvents = "C:\pagerduty\logs\prtg2pd_log.txt"
+$logEvents = "C:\Temp\prtg2pchk.log"
 
-# Send to PagerDuty and Log Results
+# Send to Pachka and Log Results
 
 $LogMtx = New-Object System.Threading.Mutex($False, "LogMtx")
 $LogMtx.WaitOne() | Out-Null
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 try {
     Invoke-RestMethod	-Method Post `
         -ContentType "application/json" `
